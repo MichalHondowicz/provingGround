1473a8368c10a1a09070c9e905b08c5fd6809de7
MichalHondowicz <40538831+MichalHondowicz@users.noreply.github.com>
2019-07-21 11:59:29 +0200

    Initial commit

---
+++ src/main/java/Anagram.java
@@ -0,0 +1,13 @@
+import java.util.List;
+
+public class Anagram {
+
+
+    public Anagram(String diaper) {
+    }
+
+    public List<String> match(List<String> asList) {
+        List<String> match = null;
+        return match;
+    }
+}
